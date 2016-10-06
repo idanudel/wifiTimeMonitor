8ceae6037834c95e4f19fa26aeeeb5af847fbcb1
idanm <Idanm@liveperson.com>
2016-10-06 22:32:18 +0300

    Starting working on wifiMonitor wifiUpdater

---
+++ app/src/main/java/com/example/idanm/wifitimemonitor/receivers/BootBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.example.idanm.wifitimemonitor.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.example.idanm.wifitimemonitor.utils.CONST;
+
+/**
+ * Created by idanm on 10/5/16.
+ */
+public class BootBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(CONST.TAG, "ON BOOT CALL START");
+        new WifiStatusUpdater(context);
+        Log.i(CONST.TAG, "ON BOOT CALL FINISH");
+    }
+}
