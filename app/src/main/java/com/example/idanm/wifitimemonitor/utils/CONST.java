ede1eb1fcf2767e4f9c81e7b0a729787f289860a
idanm <Idanm@liveperson.com>
2016-10-10 21:21:21 +0300

    Update UI from updater. Change Showwifihistory order

--- app/src/main/java/com/example/idanm/wifitimemonitor/utils/CONST.java
+++ app/src/main/java/com/example/idanm/wifitimemonitor/utils/CONST.java
@@ -6,4 +6,8 @@ package com.example.idanm.wifitimemonitor.utils;
 public class CONST {
 
     public static final String TAG ="WIFIMONITOR";
+
+
+
+    public static final String UPDATE_UI_ACTION ="UPDATE_UI_ACTION";
 }
